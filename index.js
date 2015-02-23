c9eef5103657914c3b78fc5deeb268655cd6f975
appleinventor <appleinventor14@gmail.com>
2015-02-23 12:34:32 -0800

    update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var been=document.cookie.valueOf("been");
 var Byrd={};
-if (been!=="here") {
+if (been!=="been=here") {
 	alert("Welcome to ByrdOS!");
-    document.cookie="been=here; expires=1 Dec 2100 12:00:00 UTC";
+    document.cookie="been=here; expires=1 Dec 2038 12:00:00 UTC";
 }
 else{
 	
